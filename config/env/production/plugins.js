ddbd7dca6920fe5673fbbef036ace4093de24827
strocchia <scotttrocchia@gmail.com>
2021-12-29 13:43:14 -0500

    add graphql plugin in production environ

---
+++ config/env/production/plugins.js
@@ -0,0 +1,17 @@
+// path: ./config/env/production/plugins.js
+
+module.exports = {
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
+
