a917fe8283c1476795e07076eb880cc32db30ece
strocchia <scotttrocchia@gmail.com>
2021-12-29 15:23:42 -0500

    more content types added.

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,14 @@
 // path: ./config/plugins.js
 
 module.exports = {
-  //
   graphql: {
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
     },
   },
 };
